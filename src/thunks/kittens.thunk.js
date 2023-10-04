4a66b8cca609f007ec3d4c1abad5b04ee5d82ecb
Jorge Almarales <jorgealmarales22@gmail.com>
2023-10-04 16:44:52 -0500

    fix (testing): probando nuevo registro de digital ocean

--- src/thunks/kittens.thunk.js
+++ src/thunks/kittens.thunk.js
@@ -1,9 +1,16 @@
 import { setkittens } from "../slices/kittens.slice"
 
+function getEnv() {
+    const url = window.location.href;
+    return url.includes("localhost") ? "development" : "production";
+}
+
 export const setkittensThunk = (num) => {
     return async(dispatch, getState) => {
         
-        const resp = await fetch(`http://localhost:4000/kittens`)
+        const env = getEnv()
+        const url = env === "development" ? "http://localhost:4000/kittens" : "https://kittens-back-production.up.railway.app/kittens"
+        const resp = await fetch(url)
         const data = await resp.json()
         
         dispatch(setkittens(data))
